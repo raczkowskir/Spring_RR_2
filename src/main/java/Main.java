75aa23a15b1070ae25d24b66bbc3851437520b24
Raki <raczkowskir@yahoo.com>
2021-09-01 18:07:01 +0200

    some trash

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,8 +11,17 @@ class Main implements Animals{
     public static void main(String[] args) {
         Main mainRR = new Main();
         System.out.println(mainRR.runFast(300));
+        printQuality();
     }
 
+    enum myEnum {
+        good,
+        bad,
+        noOpinion
+    }
 
+    public static void printQuality() {
+        System.out.println(myEnum.bad);
+    }
 
 }
